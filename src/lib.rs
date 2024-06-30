1da0f1ff0d5843e4646c1e37c91d267c85af5ead
DevSlashRichie <me@slashrichie.dev>
2024-06-30 13:12:20 -0600

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 //! Everything nanoid has but with a prefix.
 
-use diesel::{backend::Backend, serialize::ToSql};
-
 const SAFE: [char; 62] = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
     'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
